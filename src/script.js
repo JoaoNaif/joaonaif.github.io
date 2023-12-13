4a2f9bcbf01dd4e89bf73d34ae916f144c150739
joaonaif <joaonaif@gmail.com>
2023-12-13 00:15:09 -0300

    Home

--- src/script.js
+++ src/script.js
@@ -2,5 +2,16 @@ let bars = document.querySelector('#bars')
 
 document.querySelector('#bars').addEventListener('click', ()=>{
     document.querySelector('#header').style.height = `100vh`
+    document.querySelector('#header').style.padding = `0`
+    document.querySelector('#logo').style.display = 'none'
     bars.style.display = 'none'
+    document.querySelector('#card').style.display = 'block'
+})
+
+document.querySelector('#out').addEventListener('click', ()=>{
+    document.querySelector('#header').style.height = `40px`
+    document.querySelector('#header').style.padding = `8px`
+    document.querySelector('#logo').style.display = 'flex'
+    bars.style.display = 'flex'
+    document.querySelector('#card').style.display = 'none'
 })
