cceeb8562da2f42435cc3af4ab2783740eaa1d50
joaonaif <joaonaif@gmail.com>
2023-12-16 03:01:29 -0300

    Tech

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 let bars = document.querySelector('#bars')
+let linguage = document.querySelector('#linguage')
+let framework = document.querySelector('#framework')
+let outros = document.querySelector('#outros')
+let tudo = document.querySelector('#tudo')
 
 document.querySelector('#bars').addEventListener('click', ()=>{
     document.querySelector('#header').style.height = `100vh`
@@ -15,3 +19,39 @@ document.querySelector('#out').addEventListener('click', ()=>{
     bars.style.display = 'flex'
     document.querySelector('#card').style.display = 'none'
 })
+
+function dataTec(p){
+    document.querySelectorAll('aside').forEach((att, index)=>{
+        let attribute = att.getAttribute('data-def')
+        console.log(attribute)
+        if(p == attribute){
+            console.log('yes')
+            att.style.display = 'flex'
+        }else{
+            att.style.display = 'none'
+        }
+    })
+}
+
+linguage.addEventListener('click', ()=>{
+    let html = linguage.innerHTML
+    console.log(html)
+    dataTec(html)
+})
+
+framework.addEventListener('click', ()=>{
+    let html = framework.innerHTML
+    dataTec(html)
+})
+
+outros.addEventListener('click', ()=>{
+    let html = outros.innerHTML
+    dataTec(html)
+})
+
+tudo.addEventListener('click', ()=>{
+    document.querySelectorAll('aside').forEach((att, index)=>{
+        att.style.display = 'flex'
+    })
+})
+
