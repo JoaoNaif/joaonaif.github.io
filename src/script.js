7a6192faaf6b5c233b959bad73301fb3622c5973
joaonaif <joaonaif@gmail.com>
2023-12-18 00:42:17 -0300

    complete-80%

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ let linguage = document.querySelector('#linguage')
 let framework = document.querySelector('#framework')
 let outros = document.querySelector('#outros')
 let tudo = document.querySelector('#tudo')
+let slide = document.querySelector('#slide')
 
 document.querySelector('#bars').addEventListener('click', ()=>{
     document.querySelector('#header').style.height = `100vh`
@@ -20,10 +21,49 @@ document.querySelector('#out').addEventListener('click', ()=>{
     document.querySelector('#card').style.display = 'none'
 })
 
+document.querySelector('#menu-1').addEventListener('click', ()=>{
+    document.querySelector('#header').style.height = `40px`
+    document.querySelector('#header').style.padding = `8px`
+    document.querySelector('#logo').style.display = 'flex'
+    bars.style.display = 'flex'
+    document.querySelector('#card').style.display = 'none'
+})
+
+document.querySelector('#menu-2').addEventListener('click', ()=>{
+    document.querySelector('#header').style.height = `40px`
+    document.querySelector('#header').style.padding = `8px`
+    document.querySelector('#logo').style.display = 'flex'
+    bars.style.display = 'flex'
+    document.querySelector('#card').style.display = 'none'
+})
+
+document.querySelector('#menu-3').addEventListener('click', ()=>{
+    document.querySelector('#header').style.height = `40px`
+    document.querySelector('#header').style.padding = `8px`
+    document.querySelector('#logo').style.display = 'flex'
+    bars.style.display = 'flex'
+    document.querySelector('#card').style.display = 'none'
+})
+
+document.querySelector('#menu-4').addEventListener('click', ()=>{
+    document.querySelector('#header').style.height = `40px`
+    document.querySelector('#header').style.padding = `8px`
+    document.querySelector('#logo').style.display = 'flex'
+    bars.style.display = 'flex'
+    document.querySelector('#card').style.display = 'none'
+})
+
+document.querySelector('#menu-5').addEventListener('click', ()=>{
+    document.querySelector('#header').style.height = `40px`
+    document.querySelector('#header').style.padding = `8px`
+    document.querySelector('#logo').style.display = 'flex'
+    bars.style.display = 'flex'
+    document.querySelector('#card').style.display = 'none'
+})
+
 function dataTec(p){
     document.querySelectorAll('aside').forEach((att, index)=>{
         let attribute = att.getAttribute('data-def')
-        console.log(attribute)
         if(p == attribute){
             console.log('yes')
             att.style.display = 'flex'
@@ -33,31 +73,60 @@ function dataTec(p){
     })
 }
 
+function removeActive(){
+    let def = document.querySelectorAll('#def li')
+    for(let i of def){
+        i.classList.remove('ativo')
+    }
+
+}
+
+
+
 linguage.addEventListener('click', ()=>{
     let html = linguage.innerHTML
-    console.log(html)
     dataTec(html)
+    removeActive()
+    linguage.classList.add('ativo')
 })
 
 framework.addEventListener('click', ()=>{
     let html = framework.innerHTML
     dataTec(html)
+    removeActive()
+    framework.classList.add('ativo')
 })
 
 outros.addEventListener('click', ()=>{
     let html = outros.innerHTML
     dataTec(html)
+    removeActive()
+    outros.classList.add('ativo')
 })
 
 tudo.addEventListener('click', ()=>{
     document.querySelectorAll('aside').forEach((att, index)=>{
         att.style.display = 'flex'
     })
+    removeActive()
+    tudo.classList.add('ativo')
 })
 
 window.onload = function(){
     document.querySelectorAll('aside').forEach((att, index)=>{
         att.style.display = 'flex'
+        tudo.classList.add('ativo')
     })
 }
 
+document.querySelector('#slide-1').addEventListener('click', ()=>{
+    slide.style.marginLeft = `0`
+})
+
+document.querySelector('#slide-2').addEventListener('click', ()=>{
+    slide.style.marginLeft = `-100vw`
+})
+
+document.querySelector('#slide-3').addEventListener('click', ()=>{
+    slide.style.marginLeft = `-200vw`
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.border-gradient-1': {
+          'border-image': 'linear-gradient(to right, #68d1e9, #feb47b) 1',
+          'border-image-slice': '1',
+        },
+        '.border-gradient-2': {
+          'border-image': 'linear-gradient(to right, #68d391, #68d1e9) 1',
+          'border-image-slice': '1',
+        },
+        '.border-gradient-3': {
+          'border-image': 'linear-gradient(to right, #68d1e9, #ff0000) 1',
+          'border-image-slice': '1',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 }
 
